568b2bfdbf9e7edf99957fdd3a58b144e8fbf69e
Jorick de Lange <jorickdelange@gmail.com>
2019-11-07 22:17:51 +0100

    Create SettingsTest.php

---
+++ tests/Feature/SettingsTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class SettingsTest extends TestCase 
+{
+    /** @test */
+    function default_test()
+    {
+        $this->assertTrue(true);
+    }
+}
